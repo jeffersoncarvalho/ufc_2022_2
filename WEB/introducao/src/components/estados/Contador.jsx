9387a809814aa4a2791c837aabaa6ac6afc0ae04
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2022-09-13 19:28:12 -0300

    sena

--- WEB/introducao/src/components/estados/Contador.jsx
+++ WEB/introducao/src/components/estados/Contador.jsx
@@ -1,17 +1,40 @@
 import { useState } from "react"
 
 const Contador = ()=> {
+    
+    const [contador,setContador] = useState(100)
+    const [sena,setSena] = useState(false)
     //let contador = 0
 
     const contar = ()=> {
         //contador = contador + 1
         //console.log(contador)
+        setContador(contador + 1)
+    }
+
+    const modificarSena = ()=>{
+        //???
+    }
+
+    const renderizarSena = ()=>{
+        if(sena){
+            return(
+                <img src="https://www.quixada.ufc.br/wp-content/uploads/2015/05/David-Sena-Oliveira-Final1-240x320.png" alt="Italian Trulli"/>
+        
+            )
+        }
+        return null
     }
 
     return(
-        <div>
-            <h2>Contador </h2>
+        <div
+        >
+            <h2>Contador {contador}</h2>
             <button onClick={contar}>Acrescentar</button>
+            <button onClick={modificarSena}>Sena</button>
+            {
+                renderizarSena()
+            }
         </div>
     )
 }
