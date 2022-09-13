c13a8210fc856f67177496d8a7997c45875f41c2
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2022-09-13 19:42:45 -0300

    vota cidades

---
+++ WEB/introducao/src/components/estados/VotaCidades.jsx
@@ -0,0 +1,21 @@
+const VotaCidades = () => {
+
+    return (
+        <div>
+            <div>
+                <h2>Quixadá: </h2>
+                <h2>Limoeiro do Norte: </h2>
+                <h2>Crateús: </h2>
+            </div>
+            <div>
+                <button>Votar Quixadá</button>
+                <button>Votar Limoeiro do Norte</button>
+                <button>Votar Crateús</button>
+            </div>
+        </div>
+
+    )
+
+}
+
+export default VotaCidades
