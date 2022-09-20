c8449b0038ebe70fc330144838f806d2716d1081
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2022-09-20 19:43:07 -0300

    navbar

--- WEB/02_crud/src/index.js
+++ WEB/02_crud/src/index.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
