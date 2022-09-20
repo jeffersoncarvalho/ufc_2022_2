867ac90dc0e6f6aaa18a9a85545382f943781009
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2022-09-20 19:34:31 -0300

    navbar

--- WEB/02_crud/src/App.js
+++ WEB/02_crud/src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Routes, Route, Link} from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 
 import Home from './components/Home';
 import CreateStudent from './components/students/CreateStudent';
@@ -9,27 +9,35 @@ import ListStudent from './components/students/ListStudent';
 function App() {
   return (
     <div className='container'>
-      <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-        <Link to='/' className='navbar-brand' style={{paddingLeft:10}}>CRUD</Link>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+        <Link to='/' className='navbar-brand' style={{ paddingLeft: 10 }}>CRUD</Link>
         <div className='collapse navbar-collapse' id='navbarSupportedContent'>
           <ul className='navbar-nav mr-auto'>
             <li className='nav-item'>
               <Link to='/' className='nav-link'>Home</Link>
             </li>
-            <li className='nav-item'>
-              <Link to='/createStudent' className='nav-link'>Criar Estudante</Link>
-            </li>
-            <li className='nav-item'>
-              <Link to='/listStudent' className='nav-link'>Listar Estudante</Link>
+
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Estudante
+              </a>
+              <ul className="dropdown-menu">
+                <li className='nav-item'>
+                  <Link to='/createStudent' className='nav-link'>Criar Estudante</Link>
+                </li>
+                <li className='nav-item'>
+                  <Link to='/listStudent' className='nav-link'>Listar Estudante</Link>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
       </nav>
       <Routes>
-        <Route path='/' element={ <Home/> }/>
-        <Route path='createStudent' element={ <CreateStudent/> }/>
-        <Route path='listStudent' element={ <ListStudent/> }/>
-        <Route path='editStudent' element={ <EditStudent/> }/> 
+        <Route path='/' element={<Home />} />
+        <Route path='createStudent' element={<CreateStudent />} />
+        <Route path='listStudent' element={<ListStudent />} />
+        <Route path='editStudent' element={<EditStudent />} />
       </Routes>
     </div>
   );
--- WEB/02_crud/src/index.js
+++ WEB/02_crud/src/index.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
