adcef1db43124161d515bbc0b3120a5027e8da71
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2022-09-26 16:11:59 -0300

    Botoes e Input e Rede

--- MOBILE/introducao/components/evento/BotaoPegaTexto.jsx
+++ MOBILE/introducao/components/evento/BotaoPegaTexto.jsx
@@ -1,12 +1,50 @@
 import { View, Text, Button, TextInput, StyleSheet } from 'react-native'
+import { useState } from 'react'
 
 const BotaoPegaTexto = () => {
+
+    const [texto, setTexto] = useState('')
+
+    const acaoBotao = () => {
+        alert(texto)
+    }
+
     return (
         <View style={estilos.container}>
+
             <Text style={estilos.cabecalho}>Entrada de Texto com Botao</Text>
-            <TextInput placeholder='Digite um texto' style={estilos.input} />
+            <TextInput
+                placeholder='Digite um texto'
+                style={estilos.input}
+                onChangeText={
+                    (entrada) => {
+                        setTexto(entrada)
+                    }
+                }
+            />
+            <TextInput
+                placeholder='Digite um texto'
+                style={estilos.input}
+                onChangeText={
+                    (entrada) => {
+                        setTexto(entrada)
+                    }
+                }
+            />
+            <TextInput
+                placeholder='Digite um texto'
+                style={estilos.input}
+                onChangeText={
+                    (entrada) => {
+                        setTexto(entrada)
+                    }
+                }
+            />
             <View style={estilos.botao}>
-                <Button title='PEGAR TEXTO' />
+                <Button
+                    title='PEGAR TEXTO'
+                    onPress={acaoBotao}
+                />
             </View>
         </View>
     )
@@ -35,8 +73,7 @@ const estilos = StyleSheet.create({
     botao: {
         width: "93%",
         margin: 15,
-        backgroundColor: "red"
-
+        backgroundColor: "lightblue"
     }
 })
 export default BotaoPegaTexto
